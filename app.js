6ca0483ac02a69f4c314a18c01bce74289ade8dc
Tokio <99530989+vedanshi555@users.noreply.github.com>
2022-09-09 19:14:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
-let input = document.querySelector('.input');
-let butt = document.querySelector('.add');
-let alltasks = document.querySelector('.alltasks');
- butt.addEventListener('click', adding);
- alltasks.addEventListener('click', remove);
-// clicks.addEventListener("click", remove);
-
-
-
-
-function adding(){
-    let divisionlist = document.createElement("div");
-    let ele=document.createElement("p");
-    let text=document.createTextNode(input.value);
-    ele.appendChild(text);
-    let icon=document.createElement("img");
-    icon.classList.add("close");
-    icon.setAttribute("src","/icons8-close-24.png");
-    icon.style.width="15px";
-    divisionlist.appendChild(ele);
-    divisionlist.appendChild(icon)
-    divisionlist.classList.add("divl");
-    alltasks.appendChild(divisionlist);
-    input.value="enter task";
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-function remove(e){
-    
-     if(e.target.classList.contains("close")){
-        e.target.parentElement.remove();
-    }
-    
-    // document.getElementsByClassName(e.target.p)
-    //     let num = document.querySelector('.divl');
-         
-    // let x = document.querySelectorAll(.)
-    // let num = document.querySelector('.divl');
-    // let abv=e.target.classList.contains("close");
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-    
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
